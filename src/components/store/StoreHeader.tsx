7ec755a1b3d4cbc9eb16cf0b2a2cbbb0df58d8b5
jeffersonfortaleza123-rgb <jeffersonfortaleza123-rgb@users.noreply.github.com>
2026-09-06 22:42:51 +0000

    Aumenta o banner do cabecalho (64/80px -> 96/128px)
    Estava pequeno demais pra ver os detalhes do desenho.

--- src/components/store/StoreHeader.tsx
+++ src/components/store/StoreHeader.tsx
@@ -26,7 +26,7 @@ const StoreHeader = ({ onSearch }: StoreHeaderProps) => {
           <img
             src={settings?.logo_url || heroBanner}
             alt={settings?.store_name || "Paraíso Outlet"}
-            className="h-16 w-16 sm:h-20 sm:w-20 object-cover rounded-lg border"
+            className="h-24 w-24 sm:h-32 sm:w-32 object-cover rounded-lg border"
           />
         </Link>
 
